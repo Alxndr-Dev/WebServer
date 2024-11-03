46628d7517dab7baf5af24cfbff9cb3406194699
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-11-02 23:46:31 -0400

    Initializing Server

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+http.createServer( ( req, res )=>{
+
+    res.write('Hello World');
+    res.end();
+
+})
+
+.listen( 8080 );
+
+console.log('Server is running on port ', 8080);
